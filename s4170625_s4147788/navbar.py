1b7b0180352ea852a2f996270c97287a605e4894
S4170625 <S4170625@student.rmit.edu.au>
2025-06-12 20:25:53 +1000

    Landing page
    Rough save for landing page

---
+++ s4170625_s4147788/navbar.py
@@ -0,0 +1,12 @@
+def get_navbar_html():
+    return """
+    <nav style="background:#003366;padding:10px;">
+      <a href="/" style="color:white; margin-right:15px;">Page 1A</a>
+      <a href="/page2a" style="color:white; margin-right:15px;">Page 2A</a>
+      <a href="/page3a" style="color:white; margin-right:15px;">Page 3A</a>
+      <a href="/page1b" style="color:white; margin-right:15px;">Page 1B</a>
+      <a href="/page2b" style="color:white; margin-right:15px;">Page 2B</a>
+      <a href="/page3b" style="color:white;">Page 3B</a>
+    </nav>
+    <hr>
+    """
